d8fbb31a53cb52703f534e83e5b5639786766755
Naitian Zhou <naitian@naitian.org>
2020-07-11 12:41:38 -0400

    Dependencies and Render

--- scripts/render.js
+++ scripts/render.js
@@ -1,11 +1,33 @@
 const fs = require("fs");
 const nunjucks = require("nunjucks");
+const matter = require("gray-matter");
+const { Remarkable } = require("remarkable");
+const toc = require("markdown-toc");
 
 const config = JSON.parse(fs.readFileSync("./config.json", "utf-8"));
 
-const copy = JSON.parse(fs.readFileSync(config.archie.output));
-const template = fs.readFileSync(config.archie.template, "utf-8");
+let copy;
+let template = config[config.render_options.renderer].template;
 
+if (config.render_options.renderer === "archie") {
+  copy = JSON.parse(fs.readFileSync(config.archie.output));
+} else if (config.render_options.renderer === "markdown") {
+  md = fs.readFileSync(config.markdown.path);
+  frontmatter = matter(md);
+  copy = frontmatter.data;
+
+  copy.content = new Remarkable().use(toc.plugin).render(frontmatter.content);
+} else {
+  console.error("Unrecognized renderer");
+}
+
+/*
+ * {
+ *   hed: Headline,
+ *   by: authors,
+ *   ...other
+ * }
+ */
 nunjucks.configure({ autoescape: false });
 const output = nunjucks.renderString(template, {
   ...copy,
