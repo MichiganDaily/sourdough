038913577c3e76b140c5c2d3947476dd19119eff
Naitian Zhou <naitian@naitian.org>
2020-08-18 10:11:09 -0400

    Put into modules

--- scripts/render.js
+++ scripts/render.js
@@ -1,16 +1,21 @@
-const fs = require("fs");
-const nunjucks = require("nunjucks");
+import fs from "fs";
+import nunjucks from "nunjucks";
+
+const renderFile = (config) => {
+  const copy = JSON.parse(fs.readFileSync(config.archie.output));
+  const template = fs.readFileSync(config.archie.template, "utf-8");
+
+  nunjucks.configure({ autoescape: false });
+  const output = nunjucks.renderString(template, {
+    ...copy,
+    config,
+    env: process.env.NODE_ENV,
+  });
+
+  fs.writeFileSync("./src/index.html", output);
+};
 
 const config = JSON.parse(fs.readFileSync("./config.json", "utf-8"));
+renderFile(config);
 
-const copy = JSON.parse(fs.readFileSync(config.archie.output));
-const template = fs.readFileSync(config.archie.template, "utf-8");
-
-nunjucks.configure({ autoescape: false });
-const output = nunjucks.renderString(template, {
-  ...copy,
-  config,
-  env: process.env.NODE_ENV,
-});
-
-fs.writeFileSync("./src/index.html", output);
+export default renderFile;
